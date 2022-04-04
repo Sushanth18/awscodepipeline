7960727fb81b2160fd20af68f82a308270f52da9
sushanth shetty <sushanthshetty18@gmail.com>
2022-04-04 20:30:15 +0530

    code updated

--- src/main/java/com/mydeveloperplanet/myawscdplanet/MyAwsCdPlanetApplication.java
+++ src/main/java/com/mydeveloperplanet/myawscdplanet/MyAwsCdPlanetApplication.java
@@ -2,12 +2,18 @@ package com.mydeveloperplanet.myawscdplanet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MyAwsCdPlanetApplication {
+public class MyAwsCdPlanetApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyAwsCdPlanetApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MyAwsCdPlanetApplication.class);
+	}
 }
